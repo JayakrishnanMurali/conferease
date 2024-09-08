1153772296bb31bd573fe5c322b1a101a51a9950
Jayakrishnan M <jayakrishnan1012@gmail.com>
2024-09-08 14:28:44 +0530

    feat: Add sidebar and navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,3 +1,30 @@
+"use client";
+
+import { MobileNav } from "@/components/mobile-nav";
+import Image from "next/image";
+import Link from "next/link";
+
 export const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className="flex justify-between fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10 ">
+      <Link href="/" className="flex items-center gap-1">
+        <Image
+          src="/icons/logo.svg"
+          alt="Conferease-logo"
+          width={32}
+          height={32}
+          className="max-sm:size-10"
+        />
+        <p className="text-[26px] font-extrabold text-white max-sm:hidden">
+          Conferease
+        </p>
+      </Link>
+
+      <div className="flex justify-between gap-5">
+        {/* Clerk */}
+
+        <MobileNav />
+      </div>
+    </nav>
+  );
 };
